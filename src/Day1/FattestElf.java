4c061436dfbce77cc862741a4d50ca473748cfd9
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-12-01 03:06:34 -0500

    going through file

--- src/Day1/FattestElf.java
+++ src/Day1/FattestElf.java
@@ -1,9 +1,24 @@
 package Day1;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FattestElf {
+	int FattestElf;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			File ElvishCalories = new File("DayOneInput.txt");
+			Scanner s = new Scanner(ElvishCalories);
+			while (s.hasNextLine()) {
+				int currentElf = s.nextInt();
+				System.out.println(currentElf);
+			}
+		}
+		
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
