010aa9d2f41c87d9b3665efbbc6f3b086fc50a16
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-12-02 12:09:06 -0500

    part 2 not functioning yet but i gotta get to physics

---
+++ src/Day2/CheatinElvesPart2.java
@@ -0,0 +1,55 @@
+package Day2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CheatinElvesPart2 {
+	static int finalCount;
+	public static void main(String[] args) {
+		int valueCount[] = new int[9];
+		try {
+		File RPSInputs = new File("src/Day2/DayTwoInput.txt");
+		Scanner s = new Scanner(RPSInputs);
+
+			while(s.hasNextLine()) {
+				String currentValue = s.nextLine();
+				currentValue = currentValue.replaceAll("\\s", "");
+				if(currentValue.equals("AX")) {
+					valueCount[0]++;
+				} else if(currentValue.equals("BX")) {
+					valueCount[1]++;
+				} else if(currentValue.equals("CX")) {
+					valueCount[2]++;
+				} else if(currentValue.equals("AY")) {
+					valueCount[3]++;
+				} else if(currentValue.equals("BY")) {
+					valueCount[4]++;
+				} else if(currentValue.equals("CY")) {
+					valueCount[5]++;
+				} else if(currentValue.equals("AZ")) {
+					valueCount[6]++;
+				} else if(currentValue.equals("BZ")) {
+					valueCount[7]++;
+				} else if(currentValue.equals("CZ")) {
+					valueCount[8]++;
+				}
+				
+			}
+			finalCount += (8 * valueCount[0]);
+			finalCount += (4 * valueCount[1]);
+			finalCount += (3 * valueCount[2]);
+			finalCount += (9 * valueCount[3]);
+			finalCount += (5 * valueCount[4]);
+			finalCount += (1 * valueCount[5]);
+			finalCount += (7 * valueCount[6]);
+			finalCount += (6 * valueCount[7]);
+			finalCount += (2 * valueCount[8]);
+			System.out.println(finalCount);
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
