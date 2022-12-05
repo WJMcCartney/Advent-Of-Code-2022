5fc8f25ed0e4c3d25b23a58f62e719e01ac617df
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-12-05 15:49:40 -0500

    Finished Day 2 and adding day 3

--- src/Day2/CheatinElvesPart2.java
+++ src/Day2/CheatinElvesPart2.java
@@ -17,34 +17,36 @@ public class CheatinElvesPart2 {
 				currentValue = currentValue.replaceAll("\\s", "");
 				if(currentValue.equals("AX")) {
 					valueCount[0]++;
-				} else if(currentValue.equals("BX")) {
-					valueCount[1]++;
-				} else if(currentValue.equals("CX")) {
-					valueCount[2]++;
 				} else if(currentValue.equals("AY")) {
+					valueCount[1]++;
+				} else if(currentValue.equals("AZ")) {
+					valueCount[2]++;
+				} else if(currentValue.equals("BX")) {
 					valueCount[3]++;
 				} else if(currentValue.equals("BY")) {
 					valueCount[4]++;
-				} else if(currentValue.equals("CY")) {
-					valueCount[5]++;
-				} else if(currentValue.equals("AZ")) {
-					valueCount[6]++;
 				} else if(currentValue.equals("BZ")) {
+					valueCount[5]++;
+				} else if(currentValue.equals("CX")) {
+					valueCount[6]++;
+				} else if(currentValue.equals("CY")) {
 					valueCount[7]++;
 				} else if(currentValue.equals("CZ")) {
 					valueCount[8]++;
 				}
 				
 			}
-			finalCount += (8 * valueCount[0]);
-			finalCount += (4 * valueCount[1]);
-			finalCount += (3 * valueCount[2]);
-			finalCount += (9 * valueCount[3]);
-			finalCount += (5 * valueCount[4]);
-			finalCount += (1 * valueCount[5]);
-			finalCount += (7 * valueCount[6]);
-			finalCount += (6 * valueCount[7]);
-			finalCount += (2 * valueCount[8]);
+			System.out.println(finalCount);
+			// a rock b paper c scissors
+			finalCount += ((3+0) * valueCount[0]);//Scissors L
+			finalCount += ((1+3) * valueCount[1]);//Rock D
+			finalCount += ((2+6) * valueCount[2]);//Paper W
+			finalCount += ((1+0) * valueCount[3]);//Rock L
+			finalCount += ((2+3) * valueCount[4]);//Paper D
+			finalCount += ((3+6) * valueCount[5]);//Scissors W
+			finalCount += ((2+0) * valueCount[6]);//Paper L
+			finalCount += ((3+3) * valueCount[7]);//Scissors D
+			finalCount += ((1+6) * valueCount[8]);//Rock W
 			System.out.println(finalCount);
 		}
 		catch(FileNotFoundException e) {
